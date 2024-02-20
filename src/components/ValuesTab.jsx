15ce686518e6ca4e89a280a13ae06ac90148b089
VitaFilimonova <114240442+VitaFilimonova@users.noreply.github.com>
2024-02-20 16:07:01 +0400

    check

--- src/components/ValuesTab.jsx
+++ src/components/ValuesTab.jsx
@@ -8,14 +8,25 @@ const ValuesTab = () => {
     return (
         <div
             className={`${style.valuesTab__container} ${co2Error || tempError ? style.valuesTab__container_alarm : ''}`}>
+            {/*<div className={style.valuesTab__value}>*/}
+            {/*    <h1 className={style.valuesTab__header}>{temp} &#176;C</h1>*/}
+            {/*    <p className={style.valuesTab__description}>Температура</p>*/}
+            {/*</div>*/}
             <div className={style.valuesTab__value}>
-                <h1 className={style.valuesTab__header}>{temp} &#176;C</h1>
-                <p className={style.valuesTab__description}>Температура</p>
-            </div>
-            <div className={style.valuesTab__value}>
-                <h1 className={style.valuesTab__header}>{co2} ppm</h1>
-                <p className={style.valuesTab__description}>CO2</p>
-            </div>
+                {/*<h1 className={style.valuesTab__header}>{co2} ppm</h1>*/}
+                {/*<p className={style.valuesTab__description}>CO2</p>*/}
+                {/*<div style{{"display":"flex"}}>*/}
+
+
+                <a href="https://t.me/vitafilimonova">Открыть чат в Telegram</a>
+                <a href="skype:live:redkozubov_egor?call">Открыть чат в Skype</a>
+                <a href="https://wa.me/7927294" target="_blank" rel="noreferrer">
+                    {/*<img src="whatsapp_button_image.jpg" alt="WhatsApp" width="100" height="100">*/}
+                Whatsapp
+                </a>
+                <a href="tel:+79376536070">Позвонить</a>
+            {/*</div></div>*/}
+                </div>
         </div>
     );
 };
